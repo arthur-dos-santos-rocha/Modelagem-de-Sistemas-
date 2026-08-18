2a37ee85001e8d2f2e7603885ae25354cb94c8b7
dev-arthursr <arthurdossantosrocha2007@gmail.com>
2026-08-18 19:28:40 -0300

    Add files via upload

---
+++ Quartaatt.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.quartaatt;
+
+import java.util.Scanner;
+
+
+public class Quartaatt {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int ano; 
+       
+        System.out.println("Qual é a sua data de nascimento ");
+        ano = sc.nextInt();
+        
+
+        System.out.println("A sua idade é " + (2026 - ano));
+        
+    }
+}
