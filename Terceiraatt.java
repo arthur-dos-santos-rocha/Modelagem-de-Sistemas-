f7a57bd1273d71ceb5a81ea59006a8e86c222c3f
dev-arthursr <arthurdossantosrocha2007@gmail.com>
2026-08-18 19:10:02 -0300

    Add files via upload

---
+++ Terceiraatt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.terceiraatt;
+
+/**
+ *
+ * @author aluno.senai
+ */
+public class Terceiraatt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
